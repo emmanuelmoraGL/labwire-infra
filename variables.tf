811443b6cd0b705d50ee53daf6f0b440661cfb6e
Emmanuel Mora <emmanuel.mora@gorillalogic.com>
2022-11-09 01:51:03 -0600

    Increased resources

--- variables.tf
+++ variables.tf
@@ -66,12 +66,12 @@ variable "parzu_image_name" {
 
 variable "task_cpu" {
   description = "The number of cpu units used by the task"
-  default     = 256
+  default     = 512
 }
 
 variable "task_memory" {
   description = "The amount (in MiB) of memory used by the task"
-  default     = 1024
+  default     = 2048
 }
 
 variable "logs_retention_in_days" {
